2b48e29fbcc96cfd6a56e63a63ac348d7846d990
Zach Mosher <zmosher4@gmail.com>
2024-09-24 13:26:30 -0500

    user can delete items from cart

--- components/order/detail.js
+++ components/order/detail.js
@@ -1,26 +1,27 @@
-import Table from "../table"
+import Table from '../table';
 
 export default function CartDetail({ cart, removeProduct }) {
-  const headers = ['Product', 'Price', '']
-  const footers = ['Total', cart.total, '']
+  const headers = ['Product', 'Price', ''];
+  const footers = ['Total', cart.total, ''];
 
   return (
     <Table headers={headers} footers={footers}>
-      {
-        cart.products?.map(product => {
-          return (
-            <tr key={product.id}>
-              <td>{product.name}</td>
-              <td>{product.price}</td>
-              <td>
-                <span className="icon is-clickable" onClick={() => removeProduct(product.id)}>
-                  <i className="fas fa-trash"></i>
-                </span>
-              </td>
-            </tr>
-          )
-        })
-      }
+      {cart.lineitems?.map((item) => {
+        return (
+          <tr key={item.id}>
+            <td>{item.product.name}</td>
+            <td>{item.product.price}</td>
+            <td>
+              <span
+                className="icon is-clickable"
+                onClick={() => removeProduct(item.id)}
+              >
+                <i className="fas fa-trash"></i>
+              </span>
+            </td>
+          </tr>
+        );
+      })}
     </Table>
-  )
+  );
 }
